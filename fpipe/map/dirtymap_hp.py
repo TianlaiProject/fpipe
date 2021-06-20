04d1c8714a230a9bf55b430e70e83352c22bb800
ycli <ycli@bao.ac.cn>
2021-06-20 17:47:19 +0800

    modified:   dirtymap_hp.py 	speed up map-making

--- fpipe/map/dirtymap_hp.py
+++ fpipe/map/dirtymap_hp.py
@@ -46,6 +46,10 @@ class DirtyMap_healpix(dirtymap.DirtyMap):
         v4 = hp.ang2vec(np.pi/2 - np.radians(dec_max), np.radians(ra_min))
         self.map_pix = hp.query_polygon(self.nside, np.array([v1, v2, v3, v4]))
 
+        ra_centr, dec_centr = hp.pix2ang(self.nside, self.map_pix, lonlat=True)
+        self.ra_centr  = ra_centr  * np.pi / 180.
+        self.dec_centr = dec_centr * np.pi / 180.
+
         self.map_shp = (len(self.map_pix),)
         #axis_names = ('pix', )
         #map_tmp = np.zeros(self.map_shp, dtype=__dtype__)
@@ -138,9 +142,9 @@ class DirtyMap_healpix(dirtymap.DirtyMap):
             vis = np.abs(vis)
 
         time = ts['sec1970'][:]
-        tblock_len = 2000
+        #tblock_len = 200
         n_time, n_pol, n_bl = vis.shape
-        #tblock_len = n_time
+        tblock_len = n_time
         ff = gi[0]
         vis_idx = ((bb, pp, ff) for bb in range(n_bl) for pp in range(n_pol))
         vis_axis_names = ('bl', 'pol', 'freq')
@@ -149,6 +153,8 @@ class DirtyMap_healpix(dirtymap.DirtyMap):
         #ra_axis  = self.map_tmp.get_axis('ra')
         #dec_axis = self.map_tmp.get_axis('dec')
         pix_axis = self.map_pix
+        ra_centr = self.ra_centr
+        dec_centr = self.dec_centr
 
         map_shp = self.map_shp
         if self.params['diag_cov']:
@@ -204,7 +210,9 @@ class DirtyMap_healpix(dirtymap.DirtyMap):
                                time[st:et],
                                ra[st:et, ...],
                                dec[st:et, ...],
-                               pix_axis, self.nside,
+                               pix_axis, 
+                               #ra_centr, dec_centr,
+                               self.nside,
                                self.ra_range, self.dec_range,
                                _ci, _dm,
                                diag_cov = self.params['diag_cov'],
@@ -226,9 +234,9 @@ class DirtyMap_healpix(dirtymap.DirtyMap):
         del _ci, _dm
         gc.collect()
         
-def timestream2map(vis_one, vis_mask, vis_var, time, ra, dec, pix_axis, nside,
-                   ra_range, dec_range, cov_inv_block, dirty_map, diag_cov=False,
-                   beam_size=3./60.,  center_only=False): 
+def timestream2map(vis_one, vis_mask, vis_var, time, ra, dec, pix_axis,
+        nside, ra_range, dec_range, cov_inv_block, dirty_map, diag_cov=False,
+        beam_size=3./60.,  center_only=False): 
     
     map_shp = pix_axis.shape
     
@@ -286,13 +294,79 @@ def timestream2map(vis_one, vis_mask, vis_var, time, ra, dec, pix_axis, nside,
     dirty_map += np.dot(P.T, vis_one * weight)
 
     logger.debug('est. noise inv')
-    weight = np.eye(vis_one.shape[0]) * weight
     if diag_cov:
-        cov_inv_block += np.diag(multi_dot([P.T, weight, P]))
+        #cov_inv_block += np.diag(multi_dot([P.T, weight, P]))
+        cov_inv_block += np.dot(P.T, weight)
     else:
+        weight = np.eye(vis_one.shape[0]) * weight
         cov_inv_block += multi_dot([P.T, weight, P])
 
     del weight, P
     gc.collect()
 
-    pass
+def timestream2map_deconv(vis_one, vis_mask, vis_var, time, ra, dec,
+        ra_centr, dec_centr, nside, ra_range, dec_range, cov_inv_block, dirty_map, 
+        diag_cov=False, beam_size=3./60.,  center_only=False): 
+    
+    map_shp = ra_centr.shape
+    
+    beam_sig = beam_size  / (2. * np.sqrt(2.*np.log(2.)))
+
+    vis_mask = (vis_mask.copy()).astype('bool')
+    vis_one = np.array(vis_one)
+    vis_one[vis_mask] = 0.
+
+    _good  = ( ra  < max(ra_range))
+    _good *= ( ra  > min(ra_range))
+    _good *= ( dec < max(dec_range))
+    _good *= ( dec > min(dec_range))
+    _good *= ~vis_mask
+    if np.sum(_good) == 0: return
+
+    ra       = ra[_good]  #* np.pi / 180.
+    dec      = dec[_good] #* np.pi / 180.
+    vis_one  = vis_one[_good]
+    vis_mask = vis_mask[_good]
+    time     = time[_good]
+    vis_var  = vis_var[_good]
+    
+    logger.debug('est. pointing')
+
+    ra  = ra  * np.pi / 180.
+    dec = dec * np.pi / 180.
+
+    P = (np.sin(ra[:, None]) * np.sin(ra_centr[None, :]))\
+      + (np.cos(ra[:, None]) * np.cos(ra_centr[None, :]))\
+      * (np.cos(dec[:, None] - dec_centr[None, :]))
+
+    P  = np.arccos(P) * 180. / np.pi
+    P  = np.exp(- 0.5 * (P / beam_sig) ** 2)
+    
+
+    P.shape = (ra.shape[0], -1)
+
+    P[P < 0.1] *= 0.
+    P_norm = np.sum(P, axis=1)
+    #P_norm = np.max(P, axis=1)
+    P_norm[P_norm==0] = np.inf
+    P /= P_norm[:, None]
+
+    vis_var[vis_var==0] = np.inf #T_infinity ** 2.
+    noise_inv_weight = 1. /vis_var
+
+    weight = noise_inv_weight
+
+    logger.debug('est. dirty map')
+    dirty_map += np.dot(P.T, vis_one * weight)
+
+    logger.debug('est. noise inv')
+    if diag_cov:
+        #cov_inv_block += np.diag(multi_dot([P.T, weight, P]))
+        cov_inv_block += np.dot(P.T, weight)
+    else:
+        weight = np.eye(vis_one.shape[0]) * weight
+        cov_inv_block += multi_dot([P.T, weight, P])
+
+    del weight, P
+    gc.collect()
+
